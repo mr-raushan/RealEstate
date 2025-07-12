13250ffe38107e0f07e2b0621b6e2e779b862ece
mr-raushan <kraushan9056@gmail.com>
2025-07-12 19:26:55 +0530

    add signup controller and route

---
+++ server/controllers/User.js
@@ -0,0 +1,40 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const SignUp = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        message: "Please fill all the fields",
+        success: false,
+      });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        message: "User already exists",
+        success: false,
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(200).json({
+      message: "Account created successfully",
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express();
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 dotenv.config();
+import userRoute from "./route/user.js";
+
 const PORT = process.env.PORT || 4000;
 
 // database connection
@@ -16,6 +18,12 @@ mongoose
     console.log(error);
     process.exit(1);
   });
+
+// middleware
+app.use(express.json());
+
+// api call
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
