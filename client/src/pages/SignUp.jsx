ad5f9babc3be105efb09cbaa2f64fc379036b7f9
mr-raushan <kraushan9056@gmail.com>
2025-07-12 22:08:35 +0530

    complete signUp & signIn page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,3 +1,85 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function SignUp() {
-  return <div>SignUp</div>;
+  const loading = false;
+
+  const [input, setInput] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const changeHandler = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={submitHandler}
+        className="flex flex-col items-center justify-center gap-4 min-h-screen"
+      >
+        <h1 className="text-3xl font-bold text-center">Sign Up</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            value={input.username}
+            onChange={changeHandler}
+            className="p-1 pr-8 border border-gray-500 pl-2 rounded-md focus:outline-none bg-slate-100 w-32 sm:w-96"
+          />
+        </div>
+        <div>
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={changeHandler}
+            value={input.email}
+            className="p-1 pr-8 border border-gray-500 pl-2 rounded-md focus:outline-none bg-slate-100 w-32 sm:w-96"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            onChange={changeHandler}
+            value={input.password}
+            className="p-1 pr-8 border border-gray-500 pl-2 rounded-md focus:outline-none bg-slate-100 w-32 sm:w-96"
+          />
+        </div>
+        <div>
+          {loading ? (
+            <button className="w-32 sm:w-96 border bg-gray-700 text-white font-medium mt-2 cursor-pointer p-1 rounded-md">
+              loading...
+            </button>
+          ) : (
+            <button className="w-32 sm:w-96 border bg-gray-700 text-white font-medium mt-2 cursor-pointer p-1 rounded-md">
+              Sign Up
+            </button>
+          )}
+        </div>
+        <div className="bg-red-500 text-white font-medium mt-2 cursor-pointer p-1 text-center rounded-md w-32 sm:w-96">
+          Continue with Google
+        </div>
+        <div>
+          Have an account{" "}
+          <Link className="text-blue-500 text-sm" to={"/sign-in"}>
+            Sign In
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
